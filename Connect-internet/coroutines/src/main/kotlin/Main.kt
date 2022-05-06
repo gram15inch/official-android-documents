28ff93de469cb1eb01d9bc5b219548bfae7c626b
gram15inch <leapthestairs@gmail.com>
2022-05-07 08:59:01 +0900

    5/7

--- Connect-internet/coroutines/src/main/kotlin/Main.kt
+++ Connect-internet/coroutines/src/main/kotlin/Main.kt
@@ -1,7 +1,26 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+import kotlinx.coroutines.*
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+val formatter = DateTimeFormatter.ISO_LOCAL_TIME
+val time = { formatter.format(LocalDateTime.now()) }
+
+fun main(args: Array<String>) {
+
+    runBlocking {
+        val num1 = async { getValue() }
+        val num2 = async { getValue() }
+        println("result of num1 + num2 is ${num1.await() + num2.await()}")
+    }
+
+    //Thread.sleep(10)
+}
+
+
+suspend fun getValue(): Double {
+    println("entering getValue() at ${time()}")
+    delay(3000)
+    println("leaving getValue() at ${time()}")
+    return Math.random()
 }
